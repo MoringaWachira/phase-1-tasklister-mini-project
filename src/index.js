56021387d39d1b92a39f6ed20d55dc1fecea7e7a
first-name second-name <silvia.wachira@student.moringa.com>
2022-08-27 14:22:56 +0300

    0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildMyToDos(e.target.new_task_description.value)
+    form.reset()
+
+  })
 });
+
+
+function buildMyToDos(todo){
+  let arr = [];
+  const ul = document.getElementById("tasks")
+  ul.textContent = todo
+
+  let btn = document.createElement('button')
+  ul.appendChild(btn)
+  arr.push(todo)
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = ' clear '
+}
+
+function handleDelete(eDelete) {
+  eDelete.target.parentNode.remove()
+}
